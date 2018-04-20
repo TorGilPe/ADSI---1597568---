109bfd2a73ecd8694eb322e53ea94ef630c028e6
Victor Andres Gil Peñuela <viangipe@gmail.com>
2018-04-20 16:24:38 -0500

    Exercise number one in PHP
    print the text on the screen

---
+++ Analisis/Ejercicios en php/ExerciseOne/Points.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Exercise;
+
+class Points {
+    
+    public function One(): void{
+        echo "1. Hello, my name is Victor Andres Gil Peñuela";
+    }
+    
+    public function two(): void{
+        echo '2. This is my second menssage in PHP through object-oriented programming';
+    }  
+}
---
+++ Analisis/Ejercicios en php/ExerciseOne/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'Points.php';
+
+use Exercise\Points;
+
+$Points = new Points();
+$Points->one();
+$Points->two();
+
